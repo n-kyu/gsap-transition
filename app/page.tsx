8dca61722c6565796ef04ccfacc3bf52ed694534
n-kyu <112785244+n-kyu@users.noreply.github.com>
2024-08-29 15:34:27 -0300

    Feat: creating page with section

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Anime from '../public/anime.svg';
+
+export default function Home() {
+  return (
+   <section>
+    <h1 className="text-7xl font-extrabold text-center mt-10">Welcome</h1>
+   </section>
+  );
+}
